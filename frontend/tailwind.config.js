4374ef52e10b58b1929e553558b8053a7c20abd3
David gomez <david.gomez04@hotmail.com>
2024-06-03 03:30:36 -0500

    configuraciones y eliminacion de archivos repetidos

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        roboto: ["roboto", "sans-serif"],
+      },
+    },
+    letterSpacing:{
+      high: "0.8rem"
+    }
   },
   plugins: [],
-}
-
+};
