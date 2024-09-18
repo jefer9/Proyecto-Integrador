b34a0afbc65a474fb6ae08e3dd680eca930ff8dd
David gomez <david.gomez04@hotmail.com>
2024-09-18 18:21:36 -0500

    modelo libro

--- Backend/App/models/libro.py
+++ Backend/App/models/libro.py
@@ -1,1 +1,12 @@
-import 
+from typing import Optional
+from pydantic import BaseModel
+
+class Libro(BaseModel):
+    id: Optional[int] = None
+    titulo: str
+    autor: str
+    sinopsis: str
+    genero: str
+    añoCreacion: int
+    stock: Optional[int] = None
+    image_path: str
