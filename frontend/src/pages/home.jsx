2658092b03ee5d1a9090806a8006a60bdf596442
David gomez <david.gomez04@hotmail.com>
2024-09-18 10:50:09 -0500

    home responsive

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,31 +1,43 @@
 import Footer from "../components/footer";
 import Nav from "../components/nav";
 import Pill from "../components/pill";
-import fondoLibros from "../../public/FONDOS2/libros-fondo.webp"
-import gafas from "../../public/FONDOS2/gafas.webp"
+import fondoLibros from "../../public/FONDOS2/libros-fondo.webp";
+import gafas from "../../public/FONDOS2/gafas.webp";
 import "../styles/App.css";
 
 function Home() {
   return (
     <>
-      <div className=" min-h-screen flex flex-col">
-        <div className=" w-full flex items-center justify-between h-32">
+      <div className=" main-content flex flex-col">
+        <div className=" w-full flex items-center justify-between md:h-32 mt-4 sm:mt-0">
           {/* barra de navegacion y componente para el login y el registro */}
           <Nav />
-          <Pill />  
+          <Pill />
         </div>
-        <div className="mt-8 flex-grow flex font-roboto w-3/4 mx-auto">
+        <div className="mt-16 sm:mt-12 md:mt-4 flex-grow flex font-roboto w-3/4 mx-auto">
           <div className=" ">
-            <p className="text-[48px] md:text-[62px] font-semibold h-14 text-[var(--secondary-color)]">TU BIBLIOTECA</p>
-            <p className="text-[70px] md:text-[100px] font-black tracking-high text-[var(--primary-color)] h-32">DIGITAL</p>
-            <p className=" text-lg font-light w-4/5">
+            <p className="text-[38px] sm:text-[50px] md:text-[60px] font-semibold md:h-14 text-[var(--secondary-color)]">
+              TU BIBLIOTECA
+            </p>
+            <p className="text-[50px] sm:text-[70px] md:text-[80px] font-black tracking-high text-[var(--primary-color)] md:h-32">
+              DIGITAL
+            </p>
+            <p className=" text-lg font-light md:w-3/4">
               Sumérgete en un vasto océano de conocimiento con un solo click
             </p>
           </div>
-          <img src={fondoLibros} alt="" className=" z-10 absolute  right-0 bottom-10 fondo-libros"/>
-          <img src={gafas} alt="" className=" z-10 absolute left-16 bottom-12 gafas-img"/>
+          <img
+            src={fondoLibros}
+            alt=""
+            className=" z-10 absolute  right-0 bottom-12 fondo-libros"
+          />
+          <img
+            src={gafas}
+            alt=""
+            className=" z-10 absolute left-12 bottom-10 gafas-img"
+          />
         </div>
-        <Footer/>
+      <Footer />
       </div>
     </>
   );
